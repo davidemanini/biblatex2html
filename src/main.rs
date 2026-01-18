74e88cc23f493c8eecfa8deed0a48edf53675cf0
Davide Manini <davide@manini.cloud>
2026-01-18 15:29:48 +0100

    Use anyhow for error handling

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
 
 
-use minijinja::{context, Environment, Error};
+use minijinja::{context, Environment};
 
-use biblatex::{Bibliography, Entry, Person, PermissiveType, Date, DateValue};
+use biblatex::{Bibliography, Chunk, Date, DateValue, Entry, PermissiveType, Person, Spanned};
+//use core::fmt;
+//use std::fmt::{Display, Formatter};
+//use serde::de::Error;
 //use std::any::Any;
-use std::io::Write;
+use std::io::{Write, stdout};
 use std::{fs, io::Read};
 //use std::fs::;
 //use std::fmt::Display;
 use std::error;
+//e std::
 use serde::{Serialize, Deserialize};
 use regex::Regex;
+use anyhow::{Context, Result};
 
-//use self::CmdOptions;
 
-
-use clap::Parser;
+use clap::{Parser};
 
 
 #[derive(Parser, Debug)]
@@ -25,85 +28,84 @@ struct CmdOptions{
 
     /// HTML Template to be used
     #[arg(long)]
-    template: Option<String>,
+    template: Option<std::path::PathBuf>,
 
-    /// Incorporate Javascript inside the template
-    #[arg(long)]
-    embed_js: bool,
-
-    /// Javascript file to be used
-    #[arg(long)]
-    js_file: Option<String>,
-
-    /// Bibtex file
-    #[arg(long)]
-    bibtex: String,
-
-    /// Output file
+    /// Bibtex file (default: stdin)
     #[arg(num_args(1))]
-    output: String,
+    input: Option<std::path::PathBuf>,
+
+    /// Output file (default: stdout)
+    #[arg(num_args(1))]
+    output: Option<std::path::PathBuf>,
 }
 
 
-fn main() {
+
+
+fn get_bibliography(content: &str) -> Result<Vec<BibEntry>> {
+    let biblio = Bibliography::parse(&content).context("Fail to parse BibTeX")?;
+    Ok(biblio.iter()
+	.filter_map(|x| BibEntry::from_entry(x).ok())
+	.collect::<Vec<_>>())
+}
+    
+
+fn run_everithing(args: &CmdOptions) -> anyhow::Result<()> {
+
+    let mut input: Box<dyn Read> = match &args.input {
+	Some(input) => Box::new(
+	    fs::File::open(input)
+		.with_context(|| format!("Fail to open file {:#?}", input))?
+	),
+	None => Box::new(std::io::stdin())
+    };
+    let mut content = String::new();
+    input.read_to_string(&mut content)
+	.with_context(|| format!("Fail to read input file {:#?}", args.input))?;
+
+
+    let entries = get_bibliography(&content)?;
+
+    let mut table = include_str!("table.html").to_string();
+    if let Some(filename) = &args.template {
+	fs::File::open(filename)
+	    .with_context(|| format!("Fail to open template file {:#?}", filename))?
+	    .read_to_string(&mut table)
+	    .with_context(|| format!("Fail to read template file {:#?}", filename))?;
+    }
+
+    let html = build_html(&table, &entries)?;
+
+    let mut output: Box<dyn Write> = match &args.output {
+	Some(output) =>	Box::new(fs::File::create(output)
+				 .with_context(|| format!("Fail to open file {:#?}", output))?
+	),
+	None => Box::new(stdout()),
+    };
+    
+    output.write(html.as_bytes())
+		.with_context(|| format!("Fail to write output file {:#?}", args.output))?;
+
+    Ok(())
+}
+
+
+
+fn build_html(template: &str, entries: &Vec<BibEntry>) -> anyhow::Result<String> {
+    let mut env = Environment::new();
+    env.add_template("index.html", &template).context("Syntax error in the template")?;
+    let template = env.get_template("index.html").unwrap();
+    Ok(template.render(
+	context!{biblio => entries}
+    ).context("Fail to render template")?)
+}
+
+
+
+fn main() -> anyhow::Result<()> {
 
     let args = CmdOptions::parse();
-
-
-    let output = args.output;
-    
-    let mut input = fs::File::open(args.bibtex).unwrap();
-
-
-    let mut content = String::new();
-    input.read_to_string(&mut content).unwrap();
-
-    let biblio = Bibliography::parse(&content).unwrap();
-
-    
-
-    
-/*
-    for i in biblio {
-	let authors: Vec<String> = i.author().unwrap().iter().map(|x| person_to_string(x)).collect();
-	let auth = authors.join(", ");
-
-	let title = i.title().unwrap().iter().map(|x| x.v.get()).collect::<Vec<_>>().join("");
-	println!("{}: {}", auth, title);
-	
-}
-    */
-
-    let entries: Vec<BibEntry> = biblio.iter()
-	.filter_map(|x| BibEntry::from_entry(x).ok())
-	.collect();
-
-    dbg!(entries.len());
-    
-    for i in &entries [0..entries.len().min(4)] {
-	println!("{:?}\n", i)
-    }
-
-//    s = entries[0].serialize(true).unwrap(
-
-    let mut env = Environment::new();
-    let mut table = include_str!("table.html").to_string();
-    if let Some(filename) = args.template {
-	let mut f = fs::File::open(filename).unwrap();
-	f.read_to_string(&mut table).unwrap();
-    }
-
-
-    
-    
-    env.add_template("index.html", &table).unwrap();
-    let template = env.get_template("index.html").unwrap();
-    let html = template.render(
-	context!{biblio => entries}
-    ).unwrap();
-
-    let mut output = fs::File::create(output).unwrap();
-    output.write(html.as_bytes()).unwrap();
+    run_everithing(&args)
 }
 
 
@@ -128,8 +130,6 @@ impl Author {
 
 
 #[derive(Debug, Serialize, Deserialize)]
-//#[derive(Display)]
-//#[display(fmt = "({}, {})", x, y)]
 struct BibEntry{
     pub bib_type: String,
     pub key: String,
@@ -142,7 +142,6 @@ struct BibEntry{
     pub date: Option<String>,
     pub file: Option<String>,
     pub note: Option<String>,
-//    pub localfile: Option<String>,
 }
 
 
@@ -189,17 +188,22 @@ impl BibEntry{
 				   .map(|y| y.v.get())
 				   .collect::<Vec<_>>()
 				   .join(""))),
-	    date: date_to_string(e.date().unwrap()),
+	    date: e.date().ok().and_then(|x| date_to_string(x)),
 	    file: e.file().ok().and_then(|x| jabref_file_parse(&x).ok()),
 	    note: e.note().ok()
+		.and_then(|x| Some(chunks_to_str(x)))
+/*	    note: e.note().ok()
 		.and_then(|x| Some(x.iter()
 				   .map(|y| y.v.get())
 				   .collect::<Vec<_>>()
-				   .join(""))),
-	    
+	    .join(""))),
+	    */
 	})
     }
 }
 
 
-    
+
+fn chunks_to_str(c: &[Spanned<Chunk>]) -> String {
+    c.iter().map(|y| y.v.get()).collect::<Vec<_>>().join("")
+}
